a858a1e4b4f4a81fdd9e12028fbf0118ecac94b1
Joseph Anderson <future.logic@yahoo.com>
2023-05-13 07:02:39 -0700

    cleaned up flake8 issues with unused imports

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,6 @@
+""" Temp comment to fix flake8 linting """
+
 #from django.contrib import admin
 
 # Register your models here.
+
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,5 @@
+""" Temp comment to fix flake8 linting """
+
 #from django.db import models
 
 # Create your models here.
