aa9d6def2f163358741e9e92d55b936157685da7
Joseph Anderson <future.logic@yahoo.com>
2023-05-13 08:46:50 -0700

    run black formatter on core.models.py
    fix linting errors

--- app/core/models.py
+++ app/core/models.py
@@ -14,6 +14,7 @@ class UserManager(BaseUserManager):
     """
     Manager for users
     """
+
     def create_user(self, email, password=None, **extra_fields):
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -26,11 +27,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     """
     User in the system
     """
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
-
+    USERNAME_FIELD = "email"
