f0dc1ab9563dc6c3f08eb9085000ae5944b9737a
Joseph Anderson <future.logic@yahoo.com>
2023-05-13 12:50:46 -0700

    added tests and code for admin customization

--- app/core/models.py
+++ app/core/models.py
@@ -5,6 +5,7 @@ Database models
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
+    AbstractUser,
     BaseUserManager,
     PermissionsMixin,
 )
@@ -16,8 +17,21 @@ class UserManager(BaseUserManager):
     """
 
     def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('User must provide an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Create superuser
+        """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
