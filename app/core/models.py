2f79736add19b2a374f260c2aaaef5771550acee
Joseph Anderson <future.logic@yahoo.com>
2023-05-13 11:18:53 -0700

    added normalize email test

--- app/core/models.py
+++ app/core/models.py
@@ -16,7 +16,7 @@ class UserManager(BaseUserManager):
     """
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -33,5 +33,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     objects = UserManager()
-
     USERNAME_FIELD = "email"
