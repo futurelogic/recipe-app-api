e1a200f8439e289cb7a1400861c820ed74363502
Joseph Anderson <future.logic@yahoo.com>
2023-05-13 19:56:18 -0700

    build tests and code for user api

---
+++ app/user/views.py
@@ -0,0 +1,14 @@
+"""
+Views for the user API
+"""
+
+from rest_framework import generics
+from .serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """
+    Create a new user in the system
+    """
+
+    serializer_class = UserSerializer
