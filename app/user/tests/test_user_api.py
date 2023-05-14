7a04123a3e43ca1dbfacf66472d319c53ff40679
Joseph Anderson <future.logic@yahoo.com>
2023-05-13 20:00:20 -0700

    build tests and code for user api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -62,8 +62,14 @@ class PublicUserApiTests(TestCase):
         """
         Test an error is returned if password less than 5 chars
         """
-        payload = {"email": "test@example.com", "password": "pw", "name": "Test Name"}
+        payload = {
+            "email": "test@example.com",
+            "password": "pw",
+            "name": "Test Name"
+        }
         result = self.client.post(CREATE_USER_URL, payload)
         self.assertEquals(result.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
+        user_exists = get_user_model().objects.filter(
+            email=payload["email"]
+        ).exists()
         self.assertFalse(user_exists)
