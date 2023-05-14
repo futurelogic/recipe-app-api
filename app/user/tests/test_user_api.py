0b46bd3344d58b92b893bd4a71300d989de81cc0
Joseph Anderson <future.logic@yahoo.com>
2023-05-14 07:34:54 -0700

    init_setup: user api, build tests and code for token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -63,10 +63,16 @@ class PublicUserApiTests(TestCase):
         """
         Test for returning error if password less than 5 chars
         """
-        payload = {"email": "test@example.com", "password": "pw", "name": "Test Name"}
+        payload = {
+            "email": "test@example.com",
+            "password": "pw",
+            "name": "Test Name"
+        }
         result = self.client.post(CREATE_USER_URL, payload)
         self.assertEquals(result.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
+        user_exists = get_user_model().objects.filter(
+            email=payload["email"]
+        ).exists()
         self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
@@ -84,6 +90,7 @@ class PublicUserApiTests(TestCase):
             "password": user_details["password"],
         }
         result = self.client.post(TOKEN_URL, payload)
+        return result
 
     def test_create_token_bad_credentials(self):
         """
